75b09a47c7296551d1f44ca3de7f4c66b3550636
Raúl <raul@email>
2022-12-26 18:52:59 +0100

    minor edits in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,11 +2,12 @@
 
 ################
 # Preparation of scheduled tasks
+# Inspired from https://www.baeldung.com/linux/create-crontab-script
 ################
 
-chmod +x /home/ip2tor/scripts/host_checkin_hello.sh
-# Inspired from https://www.baeldung.com/linux/create-crontab-script
+# A simple task to check visually that cron is running
 (crontab -l; echo "* * * * * touch /home/ip2tor/logs/cron-alive") | awk '!x[$0]++' | crontab -
+# Send a HELLO (code 0) to the host every 3 minutes
 (crontab -l; echo "*/3 * * * * /usr/local/bin/ip2tor_host.sh checkin 0 'Automated HELLO from host' >> /home/ip2tor/logs/host_checkin_hello.log 2>&1") | awk '!x[$0]++' | crontab -
 
 ################

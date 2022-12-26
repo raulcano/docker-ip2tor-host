255c6e346338d4547ddd72e28c4f79b94da0141f
Raúl <raul@email>
2022-12-26 18:35:22 +0100

    adds start.sh script and tidy up the crontab stuff

--- scripts/prepare_crontab.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-chmod +x /home/ip2tor/scripts/host_checkin_hello.sh
-
-# Inspired from https://www.baeldung.com/linux/create-crontab-script
-(crontab -l; echo "3 * * * * /home/ip2tor/scripts/host_checkin_hello.sh") | awk '!x[$0]++' | crontab -
---
+++ scripts/start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+################
+# Preparation of scheduled tasks
+################
+
+chmod +x /home/ip2tor/scripts/host_checkin_hello.sh
+# Inspired from https://www.baeldung.com/linux/create-crontab-script
+(crontab -l; echo "* * * * * touch /home/ip2tor/logs/cron-alive") | awk '!x[$0]++' | crontab -
+(crontab -l; echo "3 * * * * /home/ip2tor/scripts/host_checkin_hello.sh >> /home/ip2tor/logs/host_checkin_hello.log 2>&1") | awk '!x[$0]++' | crontab -
+
+################
+# Run the task scheduler 'cron'
+################
+service cron start
+
+################
+# Run the host script in loop
+################
+/usr/local/bin/ip2tor_host.sh loop

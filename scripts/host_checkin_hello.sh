2052095ae2d8891c77670521a0072710f2a14c62
Raúl <raul@email>
2022-12-26 18:45:41 +0100

    more tidying up of the cron stuff

--- scripts/host_checkin_hello.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ip2tor_host.sh checkin 0 "Automated HELLO from host"
--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@
 chmod +x /home/ip2tor/scripts/host_checkin_hello.sh
 # Inspired from https://www.baeldung.com/linux/create-crontab-script
 (crontab -l; echo "* * * * * touch /home/ip2tor/logs/cron-alive") | awk '!x[$0]++' | crontab -
-(crontab -l; echo "3 * * * * /home/ip2tor/scripts/host_checkin_hello.sh >> /home/ip2tor/logs/host_checkin_hello.log 2>&1") | awk '!x[$0]++' | crontab -
+(crontab -l; echo "*/3 * * * * /usr/local/bin/ip2tor_host.sh checkin 0 'Automated HELLO from host' >> /home/ip2tor/logs/host_checkin_hello.log 2>&1") | awk '!x[$0]++' | crontab -
 
 ################
 # Run the task scheduler 'cron'

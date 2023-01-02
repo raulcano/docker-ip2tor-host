cdc082fa4dc73c685a058b69bbe7b19d7c94ee27
Raúl <raul@email>
2023-01-03 00:00:40 +0100

    cronjob - sync to run every 30 min

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,7 @@
 (crontab -l; echo "* * * * * touch /home/ip2tor/logs/cron-alive") | awk '!x[$0]++' | crontab -
 
 # Clean open bridges in host that do not have a corresponding entry in the shop 
-(crontab -l; echo "*/2 * * * * /usr/local/bin/ip2tor_host.sh sync >> /home/ip2tor/logs/host_sync.log 2>&1") | awk '!x[$0]++' | crontab -
+(crontab -l; echo "*/30 * * * * /usr/local/bin/ip2tor_host.sh sync >> /home/ip2tor/logs/host_sync.log 2>&1") | awk '!x[$0]++' | crontab -
 
 ################
 # Run the task scheduler 'cron'

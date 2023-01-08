81c05d2b35655dc3ee780dbe9dab4eb6fa3e5ba4
Raúl <raul@email>
2023-01-08 15:14:53 +0100

    minor fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,6 @@
 # IMPORTANT: The ip2tor_host.sh loop is sending a 'hello' every 2 seconds
 # For the moment, I see no need to have the crontab running to check in
 ################
-
 source /home/ip2tor/.env
 
 
@@ -59,8 +58,13 @@ fi
 ################
 
 # A simple task to check visually that cron is running
-(crontab -l; echo "* * * * * touch /home/ip2tor/logs/cron-alive") | awk '!x[$0]++' | crontab -
+# (runuser -l ip2tor -c 'crontab -l'; echo "* * * * * touch /home/ip2tor/logs/cron-alive") | awk '!x[$0]++' | runuser -l ip2tor -c 'crontab -'
+# # Sync active bridges with info from shop
+# (runuser -l ip2tor -c 'crontab -l'; echo "*/30 * * * * /usr/local/bin/ip2tor_host.sh sync >> /home/ip2tor/logs/host_sync.log 2>&1") | awk '!x[$0]++' | runuser -l ip2tor -c 'crontab -'
 
+# This would add the crontab to the root user
+# A simple task to check visually that cron is running
+(crontab -l; echo "* * * * * touch /home/ip2tor/logs/cron-alive") | awk '!x[$0]++' | crontab -
 # Sync active bridges with info from shop
 (crontab -l; echo "*/30 * * * * /usr/local/bin/ip2tor_host.sh sync >> /home/ip2tor/logs/host_sync.log 2>&1") | awk '!x[$0]++' | crontab -
 
